0753226840179420369458881963d89339c17ca2
Aziz Meddah <zizou4049@gmail.com>
2025-08-12 13:16:48 +0100

    fix(database): switch between synchronize & migrations efficiently

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,7 +2,6 @@ import AppConfig from './app.config';
 import DatabaseConfig from './database.config';
 import DocConfig from './doc.config';
 import i18nConfig from './i18n.config';
-import SentryConfig from './sentry.config';
 import AppPreferencesConfig from './app-preference.config';
 export default [
   AppConfig,
@@ -10,5 +9,4 @@ export default [
   DatabaseConfig,
   DocConfig,
   i18nConfig,
-  SentryConfig,
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import {
-  //HttpAdapterHost,
   NestApplication,
   NestFactory,
   Reflector,
@@ -13,8 +12,6 @@ import {
 } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
-//import * as Sentry from '@sentry/node';
-//import { SentryFilter } from './filters/sentry.filter';
 import { MigrationService } from './common/database/services/database-migration.service';
 import { join } from 'path';
 
@@ -36,8 +33,6 @@ async function bootstrap() {
   const docDesc: string = configService.get<string>('doc.description');
   const docVersion: string = configService.get<string>('doc.version');
   const docPrefix: string = configService.get<string>('doc.prefix');
-
-  //const sentryDSN: string = configService.get<string>('sentry.dsn');
 
   //Swagger ==============================================================
 
@@ -77,55 +72,36 @@ async function bootstrap() {
     explorer: true,
     customSiteTitle: docName,
   });
-
-  //Sentry ==============================================================
-
-  // Initialize Sentry by passing the DNS included in the .env
-  /*Sentry.init({
-    dsn: sentryDSN,
-  });
-  // Import the filter globally, capturing all exceptions on all routes
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new SentryFilter(httpAdapter));
-  */
-
   //Migrations ==========================================================
-  //const synchronize = configService.get<boolean>('database.synchronize');
-  //if (!synchronize) {
-  const migrationService = app.get(MigrationService);
-  const migrationPath = join(__dirname, 'migrations');
-  try {
-    // Create migrations table if it does not exist
-    await migrationService.createMigrationsTableIfNotExists();
+  const synchronize = configService.get<boolean>('database.synchronize') ;
+  if (!synchronize) {
+    const migrationService = app.get(MigrationService);
+    const migrationPath = join(__dirname, 'migrations');
+    try {
+      // Create migrations table if it does not exist
+      await migrationService.createMigrationsTableIfNotExists();
 
-    const migrationFiles =
-      await migrationService.loadMigrationFiles(migrationPath);
+      const migrationFiles =
+        await migrationService.loadMigrationFiles(migrationPath);
 
-    const existingMigrations = await migrationService.findAll({});
+      const existingMigrations = await migrationService.findAll({});
 
-    // Check if there are any migrations to run
-    const needToRunMigrations = await migrationService.runNeeded(
-      migrationFiles,
-      existingMigrations,
-    );
+      // Check if there are any migrations to run
+      const needToRunMigrations = await migrationService.runNeeded(
+        migrationFiles,
+        existingMigrations,
+      );
 
-    if (needToRunMigrations) {
-      await migrationService.runMigrations(migrationPath, migrationFiles);
+      if (needToRunMigrations) {
+        await migrationService.runMigrations(migrationPath, migrationFiles);
+      }
+    } catch (error) {
+      logger.error('Migration process failed', error.stack);
     }
-  } catch (error) {
-    logger.error('Migration process failed', error.stack);
   }
-  //}
   //===================================================================
-
-  // logger.log(`==========================================================`);
-  // logger.log(`Environment Variable`, 'NestApplication');
-  // logger.log(JSON.parse(JSON.stringify(process.env)), 'NestApplication');
-  // logger.log(`==========================================================`);
-
   await app.listen(port);
   logger.log(`==========================================================`);
-  //logger.log(`Sentry is Enabled ${sentryDSN}`);
   logger.log(`Http Server running on ${await app.getUrl()}`, 'NestApplication');
   logger.log(`==========================================================`);
 }
